fc86cf8f446d3626a3d8b412f6a553ee615b2b21
Homfay-0903 <2790165595@qq.com>
2026-02-15 12:17:34 +0800

    Ver-0.4

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -14,4 +14,47 @@ const Canvas = () => {
 
     //选取的工具
     const [selectedTool, setSelectedTool] = useState<ElementsTypes | null>(null)
+
+    //初始化画笔
+    useEffect(() => {
+        const canvas = canvasRef.current
+        if (!canvas) return
+        const ctx = canvas.getContext('2d')
+        if (!ctx) return
+
+        //清空画布
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+        //先绘制所有已固化的图形
+        elements.forEach((element) => {
+            const rc = rough.canvas(canvas)
+            if (element.type === 'rectangle') {
+                rc.draw(
+                    rc.rectangle(element.x, element.y, element.width, element.height, {
+                        roughness: 2.5,
+                        stroke: '#000'
+                    })
+                )
+            }
+
+            //todo 线条
+        })
+
+        //再画正在画的图形（预览）
+        if (drawingElement) {
+            const rc = rough.canvas(canvas)
+            if (drawingElement.type === 'rectangle') {
+                rc.draw(
+                    rc.rectangle(drawingElement.x, drawingElement.y, drawingElement.width, drawingElement.height, {
+                        roughness: 2.5,
+                        stroke: '#000'
+                    })
+                )
+            }
+        }
+    }, [elements, drawingElement])
+
+    return <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}></canvas>
 }
+
+export default Canvas
